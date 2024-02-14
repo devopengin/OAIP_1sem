37d3d715a7914b59e8d19cfc2b44e147604c00b6
devopengin <152427505+devopengin@users.noreply.github.com>
2024-02-14 20:25:40 +0300

    Add files via upload

---
+++ 4/8 вариант/Lab4-8-1/Lab4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	char c, space; space = ' ';
+	cout << "Введите символ ";
+	cin >> c;
+	cout << setw(40) << setfill(space) << space;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(39) << setfill(space) << space;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(40) << setfill(space) << space;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(41) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(37) << setfill(space) << space;
+	cout << setw(10) << setfill(c) << c << endl;
+	cout << setw(41) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(41) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(41) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(40) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c;
+	cout << setw(1) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(39) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c;
+	cout << setw(3) << setfill(space) << space;
+	cout << setw(1) << setfill(c) << c << endl;
+}
---
+++ 4/8 вариант/Lab4-8-2/Lab-8-2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	double a, b, c, p, s;
+	cout << "Введите стороны треугольника:  a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+		p = (a + b + c) / 2;
+		s = sqrt(p * (p - a) * (p - b) * (p - c));
+		cout << "Площадь треугольника = " << s << endl;
+		return 0;
+	
+}
